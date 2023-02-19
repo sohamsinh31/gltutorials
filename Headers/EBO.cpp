288fcf205e78b1a18afe684bdb44cb1a178be524
sohamsinh31 <sohamb2019@gmail.com>
2023-02-19 21:54:53 +0530

    font loaded

--- Headers/EBO.cpp
+++ Headers/EBO.cpp
@@ -3,7 +3,7 @@
 EBO :: EBO(GLfloat* indices,GLsizeiptr size)
 {
     glGenBuffers(1,&ID);
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ID);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER,size,indices,GL_STATIC_DRAW);
 }
 
--- Random/main.cpp
+++ Random/main.cpp
@@ -7,20 +7,20 @@
 #include "Headers/EBO.h"
 #include "Headers/shader.h"
 
-
 const GLuint WIDTH = 800, HEIGHT = 600;
-const GLchar *vertexShaderSource = "#version 330 core\n"
-                                   "layout (location = 0) in vec3 position;\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-                                   "}\0";
-const GLchar *fragmentShaderSource = "#version 330 core\n"
-                                     "out vec4 color;\n"
-                                     "void main()\n"
-                                     "{\n"
-                                     "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                     "}\n\0";
+
+float vertices[] = {
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+};
+
+float indices[] = {
+    // note that we start from 0!
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+};
 
 int main()
 {
@@ -45,22 +45,13 @@ int main()
     }
     glViewport(0, 0, screenWidth, screenHeight);
 
-    Shader shaderProgram("def.vert","def.frag");
-
-    GLfloat vertices[] =
-        {
-            -0.5f, -0.5f, 0.0f, // Left
-            0.5f, -0.5f, 0.0f,  // Right
-            0.0f, 0.5f, 0.0f    // Top
-        };
-
-        VAO vao1;
-        vao1.Bind();
-
-        VBO vbo1(vertices,sizeof(vertices));
-        vao1.LinkVBO(vbo1,0);
-        vao1.Unbind();
-        vbo1.Unbind();
+    Shader shaderProgram("def.vert", "def.frag");
+    VAO vao1;
+    vao1.Bind();
+    VBO vbo1(vertices, sizeof(vertices));
+    vao1.LinkVBO(vbo1, 0);
+    vao1.Unbind();
+    vbo1.Unbind();
 
     while (!glfwWindowShouldClose(window))
     {
@@ -70,13 +61,13 @@ int main()
 
         shaderProgram.Activate();
         vao1.Bind();
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
+        glDrawArrays(GL_TRIANGLES,0,3);
 
         glfwSwapBuffers(window);
     }
     vao1.Delete();
     vbo1.Delete();
+    shaderProgram.Delete();
     glfwTerminate();
 
     return EXIT_SUCCESS;
