6e75cb3f5ed32ff550bcbd966043da55e61fb206
sohamsinh31 <sohamb2019@gmail.com>
2023-02-22 19:58:51 +0530

    fixed comments

--- src/tuts/tut1_shader.cpp
+++ src/tuts/tut1_shader.cpp
@@ -23,17 +23,6 @@ GLfloat vertices[] =
         0.0f,   -0.5f * float(sqrt(3)) / 3,      0.0f,    0.8f, 0.3f, 0.02f,//inner down
         };
 
-// GLfloat vertices[] =
-//     {
-//         //cordinates                                      //colors
-//         -0.5f,  -0.5f * float(sqrt(3)) / 3,      0.0f, 
-//         0.5f,   -0.5f * float(sqrt(3)) / 3,      0.0f,    
-//         0.0f,    0.5f * float(sqrt(3)) * 2 / 3,  0.0f,   
-//         -0.5f/2,  0.5f * float(sqrt(3)) / 6,      0.0f,    
-//         0.5f/2,   0.5f * float(sqrt(3)) / 6,      0.0f,    
-//         0.0f,   -0.5f * float(sqrt(3)) / 3,      0.0f,   
-//         };
-
 GLuint indices[] =
     {
         0, 3, 5,
@@ -77,27 +66,6 @@ int main(int argc, char const *argv[])
 
     GLuint uniId = glGetUniformLocation(shaderProgram.ID,"scale");
 
-    // GLuint VAO, VBO, EBO;
-
-    // glGenVertexArrays(1, &VAO);
-    // glGenBuffers(1, &VBO);
-    // glGenBuffers(1, &EBO);
-
-    // glBindVertexArray(VAO);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    // glEnableVertexAttribArray(0);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
-    // glBindVertexArray(0);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -105,7 +73,6 @@ int main(int argc, char const *argv[])
         shaderProgram.Activate();
         glUniform1f(uniId,0.5f);
         vao1.Bind();
-        // glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
         glfwPollEvents();
         glfwSwapBuffers(window);
@@ -113,9 +80,6 @@ int main(int argc, char const *argv[])
     vao1.Delete();
     vbo1.Delete();
     ebo1.Delete();
-    // glDeleteVertexArrays(1, &VAO);
-    // glDeleteBuffers(1, &VBO);
-    // glDeleteBuffers(1, &EBO);
     shaderProgram.Delete();
     return 0;
 }
