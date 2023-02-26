1585434893516660356099c4ab3a988aafae23ac
sohamsinh31 <sohamb2019@gmail.com>
2023-02-26 12:57:28 +0530

    added 3d

---
+++ src/tuts/cameras.cpp
@@ -0,0 +1,146 @@
+#include "stdio.h"
+#include "iostream"
+#include "stdlib.h"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "string"
+#include "Headers/shader.h"
+#include "Headers/VBO.h"
+#include "Headers/VAO.h"
+#include "Headers/EBO.h"
+#include "Headers/texture.h"
+#include "math.h"
+#include "src/SOIL2/SOIL2.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+const GLuint WIDTH = 800, HEIGHT = 800;
+
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
+};
+
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
+};
+
+int main(int argc, char const *argv[])
+{
+    glfwInit();
+
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+    if (nullptr == window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if (GLEW_OK != glewInit())
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return EXIT_FAILURE;
+    }
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    Shader shaderProgram("Assets/def.vert", "Assets/def.frag");
+
+    VAO vao1;
+    vao1.Bind();
+    VBO vbo1(vertices, sizeof(vertices));
+    EBO ebo1(indices, sizeof(indices));
+    vao1.LinkAttrib(vbo1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void *)0);
+    vao1.LinkAttrib(vbo1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    vao1.LinkAttrib(vbo1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    vao1.Unbind();
+    vbo1.Unbind();
+    ebo1.Unbind();
+
+    // texture
+    Texture popcat("Assets/ff2.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    popcat.texUnit(shaderProgram, "tex0", 0);
+
+    GLuint uniId = glGetUniformLocation(shaderProgram.ID, "scale");
+
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
+
+    // Enables the Depth Buffer
+    glEnable(GL_DEPTH_TEST);
+
+    // Main while loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Specify the color of the background
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        // Clean the back buffer and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Tell OpenGL which Shader Program we want to use
+        shaderProgram.Activate();
+
+        // Simple timer
+        double crntTime = glfwGetTime();
+        if (crntTime - prevTime >= 1 / 60)
+        {
+            rotation += 0.5f;
+            prevTime = crntTime;
+        }
+
+        // Initializes matrices so they are not the null matrix
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        // Assigns different transformations to each matrix
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)WIDTH / HEIGHT, 0.1f, 100.0f);
+
+        // Outputs the matrices into the Vertex Shader
+        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+        // Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
+        glUniform1f(uniId, 0.5f);
+        // Binds texture so that is appears in rendering
+        popcat.Bind();
+        // Bind the VAO so OpenGL knows to use it
+        vao1.Bind();
+        // Draw primitives, number of indices, datatype of indices, index of indices
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+        // Swap the back buffer with the front buffer
+        glfwSwapBuffers(window);
+        // Take care of all GLFW events
+        glfwPollEvents();
+    }
+    vao1.Delete();
+    vbo1.Delete();
+    ebo1.Delete();
+    popcat.Delete();
+    shaderProgram.Delete();
+
+    glfwTerminate();
+
+    return 0;
+}
